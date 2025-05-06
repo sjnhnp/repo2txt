84ee16231b81dccad52391b810072f9a7a5499cc
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-05-06 21:06:55 +0800

    Create password-config.js

---
+++ public/js/password-config.js
@@ -0,0 +1,6 @@
+// public/js/password-config.js
+window.PASSWORD_CONFIG = {
+    localStorageKey: 'repo2txt_passwordVerified', // 为您的应用使用一个唯一的键名
+    verificationTTL: 12 * 60 * 60 * 1000,       // 密码验证有效期：12小时 (毫秒)
+    // 你可以在这里添加更多配置，如果需要的话
+};
